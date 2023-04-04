49c3530eccce647ed175a423f94233ca3a241e89
aprise11 <agnetasilta@gmail.com>
2023-04-04 10:29:20 +0300

    Fixing deploy errors

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Form from "./Form";
 import Footer from "./Footer";
 
@@ -17,43 +17,51 @@ export default function Weather() {
   };
 
   return (
-    <div className="Weather">
-      <Form />
-      <div className="row city-content">
-        <div className="col-12">
-          <h1>{weatherData.city}</h1>
+    <Fragment>
+      <div className="Weather">
+        <Form />
+        <div className="row city-content">
+          <div className="col-12">
+            <h1>{weatherData.city}</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 todays-temperature-container">
+            <span className="current-temp">{weatherData.temperature}</span>
+            <span className="units">
+              <a href="/" className="active">
+                °C
+              </a>
+              <a href="/" className="fahrenheit">
+                °F
+              </a>
+            </span>
+          </div>
+        </div>
+        <div className="row info">
+          <div className="col-6">
+            <img
+              src={weatherData.imgUrl}
+              className="main-icon"
+              alt="Clear sky"
+            />
+          </div>
+          <div className="col-6">
+            <ul>
+              <li className="date">{weatherData.date}</li>
+              <li className="condition">{weatherData.description}</li>
+              <li className="humidity-wind">
+                Humidity: {weatherData.humidity}%
+              </li>
+              <li className="humidity-wind">Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
+        </div>
+        <div className="weather-forecast"></div>
+        <div className="Footer">
+          <Footer />
         </div>
       </div>
-      <div className="row">
-        <div className="col-12 todays-temperature-container">
-          <span className="current-temp">{weatherData.temperature}</span>
-          <span className="units">
-            <a href="/" className="active">
-              °C
-            </a>
-            <a href="/" className="fahrenheit">
-              °F
-            </a>
-          </span>
-        </div>
-      </div>
-      <div className="row info">
-        <div className="col-6">
-          <img src={weatherData.imgUrl} className="main-icon" alt="Clear sky" />
-        </div>
-        <div className="col-6">
-          <ul>
-            <li className="date">{weatherData.date}</li>
-            <li className="condition">{weatherData.description}</li>
-            <li className="humidity-wind">Humidity: {weatherData.humidity}%</li>
-            <li className="humidity-wind">Wind: {weatherData.wind} km/h</li>
-          </ul>
-        </div>
-      </div>
-      <div className="weather-forecast"></div>
-      <div className="Footer">
-        <Footer />
-      </div>
-    </div>
+    </Fragment>
   );
 }
